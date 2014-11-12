cab8f5c91dba349743a82b531600b72143b5598a
Michael Aaron <michael.aaron@colorado.edu>
2014-11-11 19:12:02 -0700

    bleh

--- app_2_main.m
+++ app_2_main.m
@@ -1,18 +1,19 @@
+function main = main()
 function x = search(tdm, query, tolerance)
-  [row_n, col_n] = size(in_matrix)
+  [row_n, col_n] = size(tdm)
 	%%Cosine
-	cos_array = zeros(col_n)
+	cos_array = zeros(1,col_n)
 	for n = 1:col_n
-		numerator = dot(in_matrix(:,n), query)
-		denominator = norm(in_matrix(:,n))*norm(query)
-		cos_array = numerator/denominator
-  end
+		numerator = dot(tdm(:,n), query)
+		denominator = norm(tdm(:,n))*norm(query)
+		cos_array[1,n] = numerator/denominator
+end
   x = cos_array
 end
+	in_matrix = [1 0 0 1 0; 1 0 1 1 1; 1 0 0 1 0; 0 0 0 1 0; 0 1 0 1 1; 0 0 0 1 0];
+	query1 = [1 0 1 0 0 0]';
+	query2 = [1 0 0 0 0 0]';
+	tolerance = .5;
 
-in_matrix = [1 0 0 1 0; 1 0 1 1 1; 1 0 0 1 0; 0 0 0 1 0; 0 1 0 1 1; 0 0 0 1 0];
-query1 = [1 0 1 0 0 0]';
-query2 = [1 0 0 0 0 0]';
-tolerance = .5;
-
-search(in_matrix, query1, tolerance)
+	result = search(in_matrix, query1, tolerance)
+end
